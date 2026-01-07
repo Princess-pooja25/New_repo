9e165591ac9db7db853ac32af8f4a9f22d1eca79
Princess-pooja25 <poojapragasam2005@gmail.com>
2026-01-07 21:57:21 +0530

    commited

---
+++ grade.py
@@ -0,0 +1,14 @@
+marks = int(input("Enter student marks: "))
+
+if marks >= 95:
+    print("Grade: A+")
+elif marks >= 90:
+    print("Grade: A")
+elif marks >= 80:
+    print("Grade: B")
+elif marks >= 70:
+    print("Grade: C")
+elif marks >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
